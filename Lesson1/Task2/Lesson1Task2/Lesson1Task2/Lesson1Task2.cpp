46511697e5566df37141eaad8dfdd4e0f160a142
Krasnovid Natalya <natalyakrasnovid@yandex.ru>
2024-02-25 22:59:53 +0300

    Add Task2 and Task3

---
+++ Lesson1/Task2/Lesson1Task2/Lesson1Task2/Lesson1Task2.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+struct schet {
+    int nomer;
+    std::string name;
+    double kol;
+};
+
+void change_schet(schet* s1, int new_kol)
+{
+    s1->kol = new_kol;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::system("chcp 1251");
+    schet s;
+    int kol1;
+    std::cout << "Введите номер счёта: ";
+    std::cin >> s.nomer;
+    std::cout << std::endl<< "Введите имя владельца: ";
+    std::cin >> s.name;
+    std::cout << std::endl<< "Введите баланс: ";
+    std::cin >> s.kol;
+    std::cout << std::endl<< "Введите новый баланс: ";
+    std::cin >> kol1;
+    change_schet(&s, kol1);
+    std::cout << "Ваш счёт: " << s.name << ", " << s.nomer << ", " << s.kol << std::endl;
+    return 0;
+}
---
+++ Lesson1/Task3/Lesson1Task3/Lesson1Task3/Lesson1Task3.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+
+struct adress {
+    std::string city;
+    std::string street;
+    int dom;
+    int kvartira;
+    int index;
+};
+
+void print_adress(adress* s1)
+{
+    std::cout << "______________________"<< std::endl;
+    std::cout << "Город: " << s1->city << std::endl;
+    std::cout << "Улица: " << s1->street << std::endl;
+    std::cout << "Номер дома: " << s1->dom << std::endl;
+    std::cout << "Номер квартиры: " << s1->kvartira << std::endl;
+    std::cout << "Индекс: " << s1->index << std::endl;
+    std::cout << "______________________" << std::endl << std::endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::system("chcp 1251");
+    adress s, x;
+    s.city = "Москва";
+    s.street = "Арбат";
+    s.dom = 12;
+    s.kvartira = 8;
+    s.index = 123456;
+    x.city = "Балашиха";
+    x.street = "Ленина";
+    x.dom = 154;
+    x.kvartira = 25;
+    x.index = 321548;
+    print_adress(&s);
+    print_adress(&x);
+    return 0;
+}
