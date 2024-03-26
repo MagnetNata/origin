e8cc6cc3995ff829fc2192af19f4a9968fe475b4
Krasnovid Natalya <natalyakrasnovid@yandex.ru>
2024-03-26 18:15:25 +0300

    Add Lesson6 new

---
+++ Lesson6_/Task2/Task2/Task2/Task2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+#define SUB(a,b) (a)-(b)
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Rus");
+    std::system("chcp 1251");
+
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << (SUB(a, b)) * c << std::endl;
+    std::cout << (SUB(a, b + c)) * c << std::endl;
+
+
+    return 0;
+}
