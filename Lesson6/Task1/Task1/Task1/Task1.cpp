bb1cb29a1146e5ff1291c848fa3d211c4c296686
Krasnovid Natalya <natalyakrasnovid@yandex.ru>
2024-03-26 18:01:18 +0300

    Add Lesson6

---
+++ Lesson6/Task1/Task1/Task1/Task1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+#define MODE 1
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    std::system("chcp 1251");
+    int x, y;
+#if defined MODE
+    #if MODE==0
+        std::cout << "Работаю в режиме тренировки" << std::endl;
+    #elif MODE==1
+        std::cout << "Работаю в боевом режиме" << std::endl;
+        std::cout << "Введите число 1" << std::endl;
+        std::cin >> x;
+        std::cout << "Введите число 2" << std::endl;
+        std::cin >> y;
+        std::cout << "Результат сложения: " << add(x, y) << std::endl;
+    #else
+        std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+    #endif
+#else
+    std::cout << "Необходимо определить MODE" << std::endl;
+#endif
+
+    return 0;
+}
---
+++ Lesson6/Task2/Task2/Task2/Counterr.h
@@ -0,0 +1,13 @@
+#pragma once
+class Counter
+{
+private:
+    int x;
+public:
+    Counter();
+    Counter(int x);
+    void setX(int x);
+    void bolshe();
+    void menshe();
+    void prosmotr();
+};
