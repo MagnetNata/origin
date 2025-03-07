a9948143e0cfd9e1bb80b2438dadd0612569670c
Krasnovid Natalya <natalyakrasnovid@yandex.ru>
2025-03-07 08:50:11 +0300

    Change

--- multithreading/hw1/hw1/hw1.cpp
+++ multithreading/hw1/hw1/hw1.cpp
@@ -2,7 +2,10 @@
 #include <Windows.h>
 #include<thread>
 #include <ctime>
-void first(int start, int end)
+
+int start = 0;
+
+void first(int end)
 {
     std::cout << "Первое окно начало свою работу!\n";
     for (int i = 0; i < end; ++i)
@@ -12,7 +15,7 @@ void first(int start, int end)
     }
     std::cout << "Первое окно закончило свою работу!\n" << std::endl;
 }
-void second(int start)
+void second()
 {
     std::cout << "Второе окно начало свою работу!\n";
     do
@@ -27,14 +30,13 @@ int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    int client = 0;
     int max_ = 10;
     auto now = std::chrono::system_clock::now();
     time_t time_now = std::chrono::system_clock::to_time_t(now);
     std::cout << "Время начала работы: ";
     printf("%02d:%02d:%02d \n", (time_now / 3600 + 3) % 24, (time_now / 60 % 60), time_now % 60);
-    std::thread window1(first, client, max_);
-    std::thread window2(second, client);
+    std::thread window1(first, max_);
+    std::thread window2(second);
     window1.join();
     window2.join();
     return 0;
