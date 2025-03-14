11a18c00346ee5e73d60b3bc561042cc995d2c24
Krasnovid Natalya <natalyakrasnovid@yandex.ru>
2025-03-14 11:42:24 +0300

    Add hw3

---
+++ multithreading/hw_3_1_/hw_3_1_/hw_3_1_.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <future>
+
+using namespace std;
+
+void min_el(int begin, vector<int>& v, promise<int> prom)
+{
+	int min1 = begin;
+	for (int j = begin; j < v.size(); j++)
+	{
+		if (v.at(j) < v.at(min1))
+		{
+			min1 = j;
+		}
+	}
+	prom.set_value(min1);
+}
+
+void sort_spisok(vector<int>& v)
+{
+	int a, b;
+	for (int i = 0; i < v.size(); i++)
+	{
+		promise<int> prom;
+		future<int> fut = prom.get_future();
+		future<void> res = async(launch::async, min_el, i, ref(v), move(prom));
+		res.wait();
+		b = fut.get();
+		a = v.at(b);
+		v.at(b) = v.at(i);
+		v.at(i) = a;
+	}
+}
+
+void print_spisok(vector<int>& v)
+{
+	cout << "Список: ";
+	for (const auto& k : v)
+	{
+		cout << k << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int kol = 20;
+	vector<int> v;
+	for (int i = 0; i < kol; i++)
+	{
+		v.push_back(rand() % 100);
+	}
+	print_spisok(v);
+	sort_spisok(v);
+	print_spisok(v);
+}
---
+++ multithreading/hw_3_2_/hw_3_2_/hw_3_2_.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <list>
+#include <iterator>
+#include <future>
+#include <thread>
+
+using namespace std;
+typedef unsigned int ui;
+
+int increment_vector(int value) {
+	return value + 1;
+}
+
+template<typename Iterator, typename T>
+void paralel_for_each(Iterator first, Iterator last, T f) {
+	const ui length = distance(first, last);
+	if (!length)
+		return;
+	const ui min_per_thread = 10;
+	if (length < (2 * min_per_thread)) {
+		for_each(first, last, f);
+	}
+	else {
+		Iterator const mid_point = first + length / 2;
+		future<void> first_half = async(&paralel_for_each<Iterator, T>, first, mid_point, f);
+		future<void> last_half = async(&paralel_for_each<Iterator, T>, mid_point, last, f);
+		first_half.get();
+		last_half.get();
+	}
+}
+
+void print_spisok(vector<int>& v)
+{
+	cout << "Список: ";
+	for (const auto& k : v)
+	{
+		cout << k << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int kol = 10;
+	vector<int> v;
+	for (int i = 0; i < kol; i++)
+	{
+		v.push_back(rand() % 100);
+	}
+	print_spisok(v);
+	paralel_for_each(v.begin(), v.end(), [](int value) {std::cout << increment_vector(value) << " "; });
+}
