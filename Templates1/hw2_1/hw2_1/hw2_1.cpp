9f978ab3e6798d1c0c967bcdf80d20bc071b17bf
Krasnovid Natalya <natalyakrasnovid@yandex.ru>
2025-04-11 10:39:01 +0300

    Add hw2

---
+++ Templates1/hw2_1/hw2_1/hw2_1.cpp
@@ -0,0 +1,60 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+
+class Text {
+public:
+    virtual void render(const std::string& data) const {
+        std::cout << data;
+    }
+};
+
+class DecoratedText : public Text {
+public:
+    explicit DecoratedText(Text* text) : text_(text) {}
+    Text* text_;
+};
+
+class Paragraph : public DecoratedText {
+public:
+    explicit Paragraph(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::cout << "<p>";
+        text_->render(data);
+        std::cout << "</p>";
+    }
+};
+
+class Reversed : public DecoratedText {
+public:
+    explicit Reversed(Text* text) : DecoratedText(text) {}
+    void render(const std::string& data) const {
+        std::string reversed_data = data;
+        std::reverse(reversed_data.begin(), reversed_data.end());
+        text_->render(reversed_data);
+    }
+};
+
+class Link : public DecoratedText {
+public:
+    explicit Link(Text* text) : DecoratedText(text) {}
+    void render(const std::string& href, const std::string& data) {
+        std::cout << "<a href=" << href << ">";
+        text_->render(data);
+        std::cout << "</a>";
+    }
+};
+
+int main() {
+    auto paragraph = new Paragraph(new Text());
+    paragraph->render("Hello world");
+    std::cout << std::endl;
+
+    auto reversed = new Reversed(new Text());
+    reversed->render("Hello world");
+    std::cout << std::endl;
+
+    auto link = new Link(new Text());
+    link->render("netology.ru", "Hello world");
+    std::cout << std::endl;
+}
---
+++ Templates1/hw2_2/hw2_2/hw2_2.cpp
@@ -0,0 +1,38 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class OneShotDB : VeryHeavyDatabase {
+public:
+    explicit OneShotDB(VeryHeavyDatabase* real_object, size_t shots = 1)
+        : real_db_(real_object), remaining_shots_(shots) {}
+
+    std::string GetData(const std::string& key) noexcept {
+        if (remaining_shots_ > 0) {
+            remaining_shots_--;
+            return real_db_->GetData(key);
+        }
+        return "error";
+    }
+
+private:
+    VeryHeavyDatabase* real_db_;
+    size_t remaining_shots_;
+};
+
+int main() {
+    auto real_db = VeryHeavyDatabase();
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+
+    return 0;
+}
